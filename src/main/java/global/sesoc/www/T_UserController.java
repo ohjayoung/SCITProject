d7383672d9d4a5953797f936f409338b4d513750
ohjayoung <mja0329@naver.com>
2018-08-25 20:32:37 +0900

    Revert "Edit T_User(controller, mapper, repository, dto, xml)"
    This reverts commit 579ce8da2478bed7229308d52a89f8f80d440eb8.

--- src/main/java/global/sesoc/www/T_UserController.java
+++ src/main/java/global/sesoc/www/T_UserController.java
@@ -6,10 +6,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import global.sesoc.www.dao.T_UserRepository;
 import global.sesoc.www.dto.T_User;
@@ -20,42 +18,17 @@ public class T_UserController {
 	T_UserRepository repository;
 	@Autowired
 	SqlSession sqlSession;
-	//로그인
+
     @RequestMapping(value="/login", method=RequestMethod.POST)
     public String login(T_User user, HttpSession session, Model model) {
     	T_User t = repository.selectOne(user);
     	if(t != null) {
     		session.setAttribute("loginId", t.getUserId());
     		session.setAttribute("loginName", t.getUserName());
-    		return "user/main";
+    		return "user/userDetail";
     	}else {
     		model.addAttribute("islogined", "1");
     		return "index";
     	}
     }
-    //내프로필 화면 요청
-    @RequestMapping(value="/userDetail", method=RequestMethod.GET)
-    public String userDetail(HttpSession session, Model model) {
-    	String userId = (String) session.getAttribute("loginId");
-    	T_User user = new T_User();
-    	user.setUserId(userId);
-    	    	
-    	T_User t = repository.selectOne(user);
-    	
-    	String p1 = t.getPhone();
-    	String[] p2 = p1.split(",");
-    	String phone1 = p2[0];
-    	String phone2 = p2[1];
-    	model.addAttribute("phone1",phone1);
-    	model.addAttribute("phone2",phone2);
-    	model.addAttribute("user", t);
-    	return "user/userDetail";
-    }
-    //회원 정보 수정 처리
-    @ResponseBody
-  	@RequestMapping(value="/userUpdate", method=RequestMethod.POST)
-  	public int userUpdate(@RequestBody T_User user) {
-  		int result = repository.userUpdate(user);
-  		return result;
-  	}
 }
--- src/main/java/global/sesoc/www/dao/T_UserMapper.java
+++ src/main/java/global/sesoc/www/dao/T_UserMapper.java
@@ -6,6 +6,5 @@ public interface T_UserMapper {
 	
 	//로그인, 아이디체크
 	public T_User selectOne(T_User user);
-	//프로필 수정
-	public int userUpdate(T_User user);
+
 }
